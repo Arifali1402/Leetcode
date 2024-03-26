39b3338d93dd187ce9c7add60a4d76c0c87e3e2e
ARIF ALI <130029082+Arifali1402@users.noreply.github.com>
2024-03-26 12:21:25 +0530

    Time: 2 ms (78.15%), Space: 61.8 MB (29.96%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int mini = prices[0];
+        int profit = 0;
+        for(int i=1;i<prices.length;i++){
+            int cost = prices[i] - mini;
+            profit = Math.max(profit, cost);
+            mini = Math.min(mini, prices[i]);
+        }
+        return profit;
+    }
+}
