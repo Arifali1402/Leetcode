4b9c6ccdd090c483c13fc75197b501fca5db0ef3
ARIF ALI <130029082+Arifali1402@users.noreply.github.com>
2024-03-27 14:40:34 +0530

    Time: 49 ms (38.16%), Space: 59.7 MB (85.33%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,4 +1,48 @@
 class Solution {
+    // SPACE OPTIMIZATION
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int curr[][] = new int[2][3];
+        int after[][] = new int[2][3];
+        // Base conditions
+        // for(int idx = 0;idx<n;idx++){
+        //     for(int buy = 0;buy<2;buy++){
+        //         curr[buy][0] = 0;
+        //     }
+        // }
+        // for(int buy = 0;buy<2;buy++){
+        //     for(int capacity = 0;capacity<3;capacity++){
+        //         [buy][capacity] = 0;
+        //     }
+        // }
+        // idx == n  is in base condition
+        for(int idx = n-1; idx>=0;idx--){
+            for(int buy = 0;buy<2;buy++){
+                // capacity == 0 is in base condition
+                for(int capacity = 1;capacity<3;capacity++){
+                    int profit = 0;
+                    if(buy == 1){
+                        int take = -prices[idx] + after[0][capacity];
+                        int notTake = 0 + after[1][capacity];
+                        profit = Math.max(take, notTake);
+                    }
+                    else{
+                        int sell = prices[idx] + after[1][capacity-1];
+                        int notSell = 0 + after[0][capacity];
+                        profit = Math.max(sell, notSell);
+                    }
+                    // Return the profit
+                    curr[buy][capacity] = profit;
+                }
+            }
+            after = curr.clone();
+        }
+        return after[1][2];
+    }
+}
+/*
+// TABULATION
+class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
         int dp[][][] = new int[n+1][2][3];
@@ -37,7 +81,7 @@ class Solution {
         return dp[0][1][2];
     }
 }
-/*
+
 MEMOIZATION
 class Solution {
     private int helper(int idx, int buy, int capacity, int[] prices, int dp[][][]){
