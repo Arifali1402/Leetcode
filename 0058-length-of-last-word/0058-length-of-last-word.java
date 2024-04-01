62dd6140702096df123e9d3271e7f9f27ac6e557
ARIF ALI <130029082+Arifali1402@users.noreply.github.com>
2024-04-01 20:10:26 +0530

    Time: 1 ms (36.75%), Space: 41.6 MB (61.80%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -2,8 +2,6 @@ class Solution {
     public int lengthOfLastWord(String s) {
         String str = s.trim();
         String arr[] = str.split(" ");
-        int ans = 0;
-        int len = arr.length;
-        return arr[len-1].length();
+        return arr[arr.length-1].length();
     }
 }
