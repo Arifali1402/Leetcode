97d00ccfd7f5fe2159335e6be717a663caf2eaa9
ARIF ALI <130029082+Arifali1402@users.noreply.github.com>
2024-04-05 19:23:26 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (93.41%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
@@ -14,13 +14,47 @@
  * }
  */
 class Solution {
-    TreeNode prev = null;
+    // Recursive Solution
+    // TreeNode prev = null;
+    // public void flatten(TreeNode root) {
+    //     if(root == null) return;
+    //     flatten(root.right);
+    //     flatten(root.left);
+    //     root.right = prev;
+    //     root.left = null;
+    //     prev = root;
+    // }
+    // Iterative Solution
+    // public void flatten(TreeNode root) {
+    //     Stack<TreeNode> stack = new Stack<>();
+    //     stack.push(root);
+    //     while(!stack.isEmpty()){
+    //         TreeNode curr = stack.peek();
+    //         stack.pop();
+    //         if(curr.right != null) stack.push(curr.right);
+    //         if(curr.left != null) stack.push(curr.left);
+    //         if(!stack.isEmpty()){
+    //             curr.right = stack.peek();                
+    //         }
+    //         curr.left = null;
+    //     }
+    // }
+    // Morris Solution
     public void flatten(TreeNode root) {
-        if(root == null) return;
-        flatten(root.right);
-        flatten(root.left);
-        root.right = prev;
-        root.left = null;
-        prev = root;
+        TreeNode curr = root;
+        while(curr != null){
+            if(curr.left != null){
+                TreeNode prev = curr.left;
+                while(prev.right != null){
+                    prev = prev.right;
+                }
+                prev.right = curr.right;
+                curr.right = curr.left;
+                curr.left = null;
+            }
+            curr = curr.right;
+        }
     }
+    
+    
 }
