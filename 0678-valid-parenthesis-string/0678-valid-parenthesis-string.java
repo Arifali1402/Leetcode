b3f7447b260d3e9591c35ff86767ac6d83d151fb
ARIF ALI <130029082+Arifali1402@users.noreply.github.com>
2024-04-07 10:10:09 +0530

    Time: 0 ms (100.00%), Space: 41 MB (90.85%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        int maxi = 0;
+        int mini = 0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i) == '('){
+                // Increase the max and min by 1
+                maxi++;
+                mini++;
+            }
+            else if(s.charAt(i) == ')'){
+                // Decrease the max and min by 1
+                maxi--;
+                if(mini > 0){
+                    mini--;
+                }
+            }
+            else if(s.charAt(i) == '*'){
+                // Increase the max by 1 and decrease min by 1
+                maxi++;
+                if(mini > 0){
+                    mini--;
+                }
+            }
+            // If maxi becomes negative at any point, then it is not valid
+            if(maxi < 0){
+                return false;
+            }
+        }
+        return mini == 0;
+    }
+}
