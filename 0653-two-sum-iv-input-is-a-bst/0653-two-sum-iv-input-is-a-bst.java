89cfe94c02a08626c04fb51f83a1bee6a2283476
ARIF ALI <130029082+Arifali1402@users.noreply.github.com>
2024-04-09 21:02:29 +0530

    Time: 2 ms (94.42%), Space: 45 MB (60.47%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
@@ -44,3 +44,83 @@ class Solution {
         return false;
     }
 }
+
+/*
+class BSTIterator {
+    // Maintain a stack to store all the nodes of the BST
+    Stack<TreeNode> stack = new Stack<>();
+    
+    // reverse --> true --> before
+    // reverse --> false --> next
+
+    boolean reverse = true;
+    
+    // Initially push all the left side element of the root to the stack
+    public BSTIterator(TreeNode root, boolean isReverse) {
+        reverse = isReverse;
+        pushAll(root);
+    }
+    
+    public int next() {
+        // Mainatain a temporary node which will the top of the stack
+        TreeNode temp = stack.pop();
+        if(reverse == false){
+            // Push all the right side element of the temp to the stack
+            pushAll(temp.right);
+        }
+        else{
+            // Push all the left side element of the temp to the stack
+            pushAll(temp.left);
+        }
+        return temp.val;
+    }
+    
+    // Define a function which will push all the left/right side element of the given node
+    private void pushAll(TreeNode root){
+        while(root != null){
+            stack.push(root);
+            if(reverse = true){
+                root = root.right;
+            }
+            else{
+                root = root.left;
+            }
+        }        
+    }
+    
+    // If the stack is not empty that means it still has next element
+    public boolean hasNext() {
+        if(stack.isEmpty() == true){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+}
+
+class Solution {
+    public boolean findTarget(TreeNode root, int k) {
+        if(root == null){
+            return false;
+        }
+        BSTIterator l = new BSTIterator(root, false);
+        BSTIterator r = new BSTIterator(root, true);
+        
+        int i = l.next();
+        int j = r.next();
+        while(i < j){
+            if(i + j == k){
+                return true;
+            }
+            else if(i + j < k){
+                i = l.next();
+            }
+            else{
+                j = r.next();
+            }
+        }
+        return false;
+    }
+}
+*/
