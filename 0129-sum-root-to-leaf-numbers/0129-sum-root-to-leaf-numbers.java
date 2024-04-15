5d4b739bb492722e322e12f3ff53703d5d118e5c
ARIF ALI <130029082+Arifali1402@users.noreply.github.com>
2024-04-15 20:00:35 +0530

    Time: 2 ms (16.30%), Space: 40.7 MB (86.84%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
@@ -15,34 +15,48 @@
  */
 class Solution {
     private void dfs(TreeNode root, Stack<Integer> stack, List<Integer> list){
+        // If we encounter null, then just return
         if(root == null){
             return;
         }
+        // push the current node's val
         stack.push(root.val);
+        // If the current node does not have any left and right nodes then do the followinf operations
         if(root.left == null && root.right == null){
             int sum = 0;
             int i = 1;
+            // Store all the values of the Stack wihout deleting any value from the stack
             List<Integer> temp = new LinkedList<>(stack);
+            // Add it 
             for(int k = temp.size()-1;k>=0;k--){
                 sum = sum + temp.get(k)*i;
                 i = i*10;
             }
+            // Store the sum 
             list.add(sum);
         }
+        // else call for left and right child of the current node
         else{
             dfs(root.left, stack, list);
             dfs(root.right, stack, list);
         }
+        // for backtracking
         stack.pop();
     }
     public int sumNumbers(TreeNode root) {
+        // for storing the values of the tree
         Stack<Integer> stack = new Stack<>();
+        // to store the sum from the root to the leaf node
         List<Integer> list = new LinkedList<>();
+        // This traversal is needed for calculating the sum
         dfs(root, stack, list);
+        // Our main answer
         int answer = 0;
+        // Add all the elements from the list
         for(int l: list){
             answer += l;
         }
+        // return the answer
         return answer;
     }
 }
