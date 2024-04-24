cc0ba41f331bc45b44d908b7119e297ffeba1fdf
ARIF ALI <130029082+Arifali1402@users.noreply.github.com>
2024-04-24 10:58:12 +0530

    Time: 42 ms (52.04%), Space: 44.3 MB (39.11%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,4 +1,23 @@
 class Solution {
+    // Optimized Tabulation
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int dp[] = new int[n];
+        Arrays.fill(dp, 1);
+        int maxi = Integer.MIN_VALUE;
+        for(int idx = 0;idx<n;idx++){
+            for(int prev = 0;prev<idx;prev++){
+                if(nums[prev] < nums[idx]){
+                    dp[idx] = Math.max(dp[idx], 1 +dp[prev]);
+                }
+            }
+            maxi = Math.max(maxi, dp[idx]);
+        }
+        return maxi;
+    }
+}
+/*
+class Solution {
     // Using Binary Search
     public int lengthOfLIS(int[] nums) {
         List<Integer> list = new ArrayList<>();
