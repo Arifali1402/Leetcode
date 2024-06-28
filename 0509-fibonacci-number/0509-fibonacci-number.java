85e1094293a221b12222990217468a4ac421da1d
ARIF ALI <130029082+Arifali1402@users.noreply.github.com>
2024-06-29 02:39:46 +0530

    Time: 0 ms (100.00%), Space: 39.7 MB (95.42%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int fib(int n) {
+        if(n == 0){
+            return 0;
+
+        }
+        if(n == 1 || n == 2){
+            return 1;
+        }
+        int a = 0;
+        int b = 1;
+        for(int i = 2;i<=n;i++){
+            int c = a+b;
+            a = b;
+            b = c;
+        }
+        return b;
+    }
+}
