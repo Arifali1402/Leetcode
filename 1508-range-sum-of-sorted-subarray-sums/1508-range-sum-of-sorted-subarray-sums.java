8229a5c15f786011e27298f8234a21c403f56ec7
ARIF ALI <130029082+Arifali1402@users.noreply.github.com>
2024-08-04 18:25:09 +0530

    Time: 65 ms (72.52%), Space: 46.3 MB (52.70%) - LeetHub

---
+++ 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int rangeSum(int[] nums, int n, int left, int right) {
+        int size = (n*(n+1))/2;
+        int newArr[] = new int[size];
+        int k=0;
+        for(int i=0;i<n;i++){
+            int sum = nums[i];
+            newArr[k++] = sum;
+            for(int j=i+1;j<n;j++){
+                sum += nums[j];
+                newArr[k++] = sum;
+            }
+        }
+        Arrays.sort(newArr);
+        long ans = 0;
+        while(left <= right){
+            ans = (ans + newArr[left-1]);
+            left++;
+        }
+        return (int)((double)ans%(1e9+7));
+    }
+}
