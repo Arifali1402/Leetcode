46c848c9cd9e9e2ac8826b4cd44908a8647af64e
ARIF ALI <130029082+Arifali1402@users.noreply.github.com>
2024-08-10 16:15:18 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (43.51%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,6 +1,32 @@
 class Solution {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+        int zeroes = 0;
+        int ones = 0;
+        int twos = 0;
+        for(int element : nums){
+            if(element == 0){
+                zeroes++;
+            }
+            else if(element == 1){
+                ones++;
+            }
+            else{
+                twos++;
+            }
+        }
+        int idx=0;
+        for(int i=0;i<zeroes;i++){
+            nums[idx] = 0;
+            idx++;            
+        }
+        for(int i=0;i<ones;i++){
+            nums[idx] = 1;
+            idx++;            
+        }
+        for(int i=0;i<twos;i++){
+            nums[idx] = 2;
+            idx++;            
+        }
         return;
     }
 }
