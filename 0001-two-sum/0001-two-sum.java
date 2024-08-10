f819edf8cf088614f16c03d47e45d9cca49d891b
ARIF ALI <130029082+Arifali1402@users.noreply.github.com>
2024-08-10 15:44:06 +0530

    Time: 45 ms (30.11%), Space: 45.2 MB (8.98%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,15 +1,12 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int ans[] = new int[2];
-                for(int i = 0;i<nums.length-1;i++){
-                            for(int j = i+1;j<nums.length;j++){
-                                            if(nums[i]+nums[j] == target){
-                                                                ans[0] = i;
-                                                                                    ans[1] = j;
-                                                                                                        break;
-                                                                                                                        }
-                                                                                                                                    }
-                                                                                                                                            }
-                                                                                                                                                    return ans;
+        for(int i=0;i<nums.length-1;i++){
+            for(int j=i+1;j<nums.length;j++){
+               if(nums[i]+nums[j] == target){
+                    return new int[]{i,j};
+               } 
+            }
+        }
+        return new int[]{-1,-1};
     }
 }
